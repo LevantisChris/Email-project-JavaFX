0cc2228bbf112cf5c0924f57c1c37be61efc9a66
LevantisChris <levantischris@gmail.com>
2023-09-24 10:54:14 +0300

    9v

---
+++ src/com/mainpack/view/LoginWindowController.java
@@ -0,0 +1,24 @@
+package com.mainpack.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("Clicked");
+    }
+
+}

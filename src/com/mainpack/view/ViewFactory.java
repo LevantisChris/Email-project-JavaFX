c3d10590d188e4cc90e2b1c0c2075b46f59be3c0
LevantisChris <levantischris@gmail.com>
2023-09-28 08:35:21 +0300

    15 finished

--- src/com/mainpack/view/ViewFactory.java
+++ src/com/mainpack/view/ViewFactory.java
@@ -1,6 +1,15 @@
 package com.mainpack.view;
 
 import com.mainpack.EmailManager;
+import com.mainpack.controller.BaseController;
+import com.mainpack.controller.LoginWindowController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -12,6 +21,21 @@ public class ViewFactory {
 
     public void showLoginWindow() {
         System.out.println("Show Login window called ...");
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
